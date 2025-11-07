d62e83b87e76a4da112b4ac651df862189e9f8eb
LUMINET\corbin.hoang <corbin.hoang@sabawork.com>
2025-11-07 11:16:38 +0700

    change worker config

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   retries: process.env.CI ? 1 : 0,
   
   // Parallel workers
-  workers: process.env.CI ? 2 : 1,
+  workers: process.env.CI ? 1 : 1,
   
   // Reporter configuration
   reporter: [
